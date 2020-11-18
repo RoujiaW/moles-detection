b746c4df7185ea156cbbf9365b4d84ca99950fe7
Roujia Wang <42785242+RoujiaW@users.noreply.github.com>
2020-11-18 17:26:39 -0500

    Add files via upload

--- Core/utils.py
+++ Core/utils.py
@@ -16,6 +16,41 @@ def read_class_names(class_file_name):
             names[ID] = name.strip('\n')
     return names
 
+def load_weights(model, weights_file):
+    layer_size = 110
+    output_pos = [93, 101, 109]
+    wf = open(weights_file, 'rb')
+    major, minor, revision, seen, _ = np.fromfile(wf, dtype=np.int32, count=5)
+
+    j = 0
+    for i in range(layer_size):
+        conv_layer_name = 'conv2d_%d' %i if i > 0 else 'conv2d'
+        bn_layer_name = 'batch_normalization_%d' %j if j > 0 else 'batch_normalization'
+
+        conv_layer = model.get_layer(conv_layer_name)
+        filters = conv_layer.filters
+        k_size = conv_layer.kernel_size[0]
+        in_dim = conv_layer.input_shape[-1]
+
+        if i not in output_pos:
+            bn_weights = np.fromfile(wf, dtype=np.float32, count=4 * filters)
+            bn_weights = bn_weights.reshape((4, filters))[[1, 0, 2, 3]]
+            bn_layer = model.get_layer(bn_layer_name)
+            j += 1
+        else:
+            conv_bias = np.fromfile(wf, dtype=np.float32, count=filters)
+
+        conv_shape = (filters, in_dim, k_size, k_size)
+        conv_weights = np.fromfile(wf, dtype=np.float32, count=np.product(conv_shape))
+        conv_weights = conv_weights.reshape(conv_shape).transpose([2, 3, 1, 0])
+
+        if i not in output_pos:
+            conv_layer.set_weights([conv_weights])
+            bn_layer.set_weights(bn_weights)
+        else:
+            conv_layer.set_weights([conv_weights, conv_bias])
+    wf.close()
+
 def load_config():
     STRIDES = np.array(cfg.YOLO.STRIDES)
     ANCHORS = get_anchors(cfg.YOLO.ANCHORS)
